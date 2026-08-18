01f80f67518a83ded4a33cd374a3da8d9604429b
AbdelrahmanMahdi <abdomahdy222111@gmail.com>
2026-08-18 17:03:47 +0300

    added the first part of the header

---
+++ src/components/header/BottomHeader.jsx
@@ -0,0 +1,11 @@
+import 'react'
+
+function BottomHeader() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BottomHeader
---
+++ src/components/header/TopHeader.jsx
@@ -0,0 +1,36 @@
+import  'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../img/logo.png'
+import { FaSearch } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import './header.css'
+
+function TopHeader() {
+  return (
+    <div className='top_header'>
+        <div className="container">
+            <Link className='logo' to="/" ><img src={Logo} alt="Logo" /></Link>
+
+            <form action="" className="search-box">
+                <input type="text" name='search' id='search' placeholder='Search For Products'/>
+                <button type='submit'><FaSearch /></button>
+            </form>
+
+            <div className="header-icons">
+                <div className="icon">
+                    <FaHeart />
+                    <span className='counter'>0</span>
+                </div>
+
+                <div className="icon">
+                    <FaShoppingCart />
+                    <span className='counter'>0</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TopHeader
